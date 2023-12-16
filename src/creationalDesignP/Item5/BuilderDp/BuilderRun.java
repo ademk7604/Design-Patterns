01ae79d1270975a33ac0d1a9634ff955f75f6dd0
adem kok <ademk7604@gmail.com>
2023-12-16 11:56:11 +0100

    BuilderDp

---
+++ src/creationalDesignP/Item5/BuilderDp/BuilderRun.java
@@ -0,0 +1,16 @@
+package creationalDesignP.Item5.BuilderDp;
+
+public class BuilderRun {
+	public static void main(String[] args) {
+		/*
+		// bu uzun cozum ve tavsiye edilmiyor,
+		// 40 tne variable olsa yazmak zorlasir.
+		Product product1 = new Product();
+		product1.setId(12L);
+		product1.setName("Name");
+		product1.setPrice(100);
+		*/
+		
+	}
+
+}
